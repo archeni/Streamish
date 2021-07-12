e36e398de2b7ebadf643ad371576b3edbaed286e
Traye <johnson.traye@yahoo.com>
2021-07-12 10:57:05 -0500

    added getwithcomments into the videomanager

--- Streamish/client/src/components/Video.js
+++ Streamish/client/src/components/Video.js
@@ -2,24 +2,27 @@
 import { Card, CardBody } from "reactstrap";
 
 const Video = ({ video }) => {
-    return (
-        <Card >
-            <p className="text-left px-2">Posted by: {video.userProfile.name}</p>
-            <CardBody>
-                <iframe className="video"
-                    src={video.url}
-                    title="YouTube video player"
-                    frameBorder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen />
+  return (
+    <Card >
+      <p className="text-left px-2">Posted by: {video.userProfile.name}</p>
+      <CardBody>
+        <iframe className="video"
+          src={video.url}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen />
 
-                <p>
-                    <strong>{video.title}</strong>
-                </p>
-                <p>{video.description}</p>
-            </CardBody>
-        </Card>
-    );
+        <p>
+          <strong>{video.title}</strong>
+        </p>
+        <p>{video.description}</p>
+        <p>Comments</p>
+        <p>{video.comments.map(comment =>
+          comment.message)}</p>
+      </CardBody>
+    </Card>
+  );
 };
 
 export default Video;
--- Streamish/client/src/modules/videoManager.js
+++ Streamish/client/src/modules/videoManager.js
@@ -1,16 +1,22 @@
 ﻿const baseUrl = '/api/video';
 
 export const getAllVideos = () => {
-    return fetch(baseUrl)
-        .then((res) => res.json())
+  return fetch(baseUrl)
+    .then((res) => res.json())
+};
+
+export const GetWithComments = () => {
+  return fetch(`${baseUrl}/GetWithComments`)
+    .then((res) => res.json())
 };
 
 export const addVideo = (video) => {
-    return fetch(baseUrl, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(video),
-    });
+  return fetch(baseUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(video),
+  });
 };
+
